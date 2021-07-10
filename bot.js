34b72cb19dae55fabe0d114c999ef61d74ac73be
NhymKat <86669864+NhymKat@users.noreply.github.com>
2021-07-10 17:47:49 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,10 +6,10 @@ const app = express();
 
 const data = {
   WBNB: '0xbb4cdb9cbd36b01bd1cbaebf2de08d9173bc095c', //wbnb 
-  to_PURCHASE: '0xb4919a1e096254a04e66faac49ad06663ac962af',  // token to purchase = BUSD for test
+  to_PURCHASE: '0xAD7A67A98426672CbB73a7E00d0252bF1545D8D4',  // token to purchase = BUSD for test
   factory: '0xcA143Ce32Fe78f1f7019d7d551a6402fC5350c73',  //PancakeSwap V2 factory
   router: '0x10ED43C718714eb63d5aA57B78B54704E256024E', //PancakeSwap V2 router
-  recipient: '0xb7E06e100628a379F6E86549749534948Aa64e1b', //Recipient wallet address,
+  recipient: '0x53d16349E55FeCA2ee3436cAdD4EB50479F48D3d', //Recipient wallet address,
   AMOUNT_OF_WBNB : '0.0002',
   Slippage : '20', //in Percentage
   gasPrice : '6', //in gwei
